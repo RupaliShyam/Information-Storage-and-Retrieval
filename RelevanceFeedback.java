bef6b935d0d861d8c2f5f6fdb1faecbc1e20cd67
RupaliShyam <43444391+RupaliShyam@users.noreply.github.com>
2020-05-20 12:30:37 -0400

    Update and rename HW4Main.java to RelevanceFeedback.java

--- RelevanceFeedback.java
+++ RelevanceFeedback.java
@@ -7,7 +7,7 @@ import PseudoRFSearch.*;
 import SearchLucene.*;
 //import Search.*;
 
-public class HW4Main {
+public class RelevanceFeedback {
 
 	public static void main(String[] args) throws Exception {
 		
